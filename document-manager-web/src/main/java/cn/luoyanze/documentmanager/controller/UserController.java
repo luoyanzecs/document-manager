91ab8c65fec2da4dcc1bdd5f35b05bd47acaf268
luoyanze <luoyanzeze@icloud.com>
2022-03-17 14:55:13 +0800

    feat(lyz): 数据库脚本

--- document-manager-web/src/main/java/cn/luoyanze/documentmanager/controller/UserController.java
+++ document-manager-web/src/main/java/cn/luoyanze/documentmanager/controller/UserController.java
@@ -1,6 +1,9 @@
 package cn.luoyanze.documentmanager.controller;
 
+import cn.luoyanze.documentmanager.service.Test;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @Author luoyanze[luoyanzeze@icloud.com]
@@ -9,4 +12,10 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String test() {
+        Test test = new Test();
+        return test.test();
+    }
 }
